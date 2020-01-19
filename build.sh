5fcae4ad35c06a70642548aa84b8df4bfdac24c7
spikeburton <ralphburton3@gmail.com>
2020-01-19 00:42:06 -0500

    add set -e as first command; add name/hostname for container

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+set -e
+
 DOCKER_IMAGE=spikeburton/jupyter
 docker build -t $DOCKER_IMAGE .
--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+set -e
+
 # Original command this image/script is built from 👇
 # docker run -t --rm -p 8888:8888 -v $(PWD)/notebooks:/opt/notebooks continuumio/miniconda3 /bin/bash -c "/opt/conda/bin/conda install jupyter -y --quiet && /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks --ip=0.0.0.0 --port=8888 --no-browser --allow-root"
+
 DOCKER_IMAGE=spikeburton/jupyter
 NOTEBOOKS=$(PWD)/notebooks
 if ! docker images | grep -q $DOCKER_IMAGE; then
   docker build -t $DOCKER_IMAGE .
 fi
-docker run -t --rm -p 8888:8888 -v $NOTEBOOKS:/opt/notebooks $DOCKER_IMAGE
+docker run -t --rm --name jupyter --hostname jupyter -p 8888:8888 -v $NOTEBOOKS:/opt/notebooks $DOCKER_IMAGE
